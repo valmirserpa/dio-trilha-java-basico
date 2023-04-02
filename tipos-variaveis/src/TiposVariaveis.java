f187d1b16f24c79afe38664279b82abcb1f0654e
Valmir <valmirleitejunior@gmail.com>
2023-04-02 20:35:17 -0300

    Concaternação

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -4,14 +4,27 @@ public class TiposVariaveis {
 
 
         final int NUMERO = 5; // Quando uma variável possui valor, devemos escrever "final" antes do tipo e nome em caixa alta
-    
-
-
         System.out.println(NUMERO);
 
+        String nomeComposto = "Valmir " + "Serpa";
+        System.out.println(nomeComposto);
 
+        String concatenacao ="?"; 
 
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
 
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
 
 
 
