a5756408cc4d1807a8c7b52991e08eb842f17e74
Valmir <valmirleitejunior@gmail.com>
2023-04-02 20:46:48 -0300

    Unários

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+
+        int numero = 5;
+        numero = - numero;
+
+        System.out.println(-numero);
+        System.out.println(numero);
+
+        numero = -1*numero;
+        System.out.println(numero);
+
+    }
+}
