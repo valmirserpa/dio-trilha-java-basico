483efbaa4b1fb3c41a6ee9ddb75720ee6d2b5a7b
Valmir <valmirleitejunior@gmail.com>
2023-04-02 20:09:09 -0300

    Variáveis constantes

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,20 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        
+
+
+        final int NUMERO = 5; // Quando uma variável possui valor, devemos escrever "final" antes do tipo e nome em caixa alta
+    
+
+
+        System.out.println(NUMERO);
+
+
+
+
+
+
+
+
+    }
+}
