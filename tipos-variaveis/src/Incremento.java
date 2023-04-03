9b3924e1531e050271b250b2dfcc642168becb89
Valmir <valmirleitejunior@gmail.com>
2023-04-02 21:36:37 -0300

    Incremento

---
+++ tipos-variaveis/src/Incremento.java
@@ -0,0 +1,21 @@
+public class Incremento {
+    public static void main(String[] args) {
+        
+        int numero = 5;
+ 
+        System.out.println(++ numero);
+        System.out.println(-- numero);
+        
+        System.out.println(numero ++);
+        System.out.println(numero --);
+
+
+        int a = 5;
+        int b = ++ a;
+        System.out.println(b);
+
+    
+
+
+    }
+}
