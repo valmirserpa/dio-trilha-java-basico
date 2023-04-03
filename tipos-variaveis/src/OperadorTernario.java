380e6688b7366026240068a2f559aa43a4eb88e8
Valmir <valmirleitejunior@gmail.com>
2023-04-03 07:49:05 -0300

    Operador ternário

---
+++ tipos-variaveis/src/OperadorTernario.java
@@ -0,0 +1,48 @@
+public class OperadorTernario {
+    public static void main(String[] args) {
+        
+
+        int a, b;
+
+        a=5;
+        b=6;
+
+        String resultado = "";
+
+        if (a==b) {
+            resultado="verdadeiro";
+        } else {
+            resultado="falso";
+        }
+
+        System.out.println(resultado);
+
+
+        int c, d;
+
+        c=5;
+        d=5;
+
+        String resultado1 = "";
+
+        if (c==d) {
+            resultado1="verdadeiro";
+        } else {
+            resultado1="falso";
+        }
+
+        System.out.println(resultado1);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
