708b17841a49129acb99e3e12d1ebc52205bcea8
Valmir <valmirleitejunior@gmail.com>
2023-04-03 09:09:15 -0300

    Operadores Lógicos

---
+++ Operadores/Operadores/src/OperadoresLogicos.java
@@ -0,0 +1,29 @@
+public class OperadoresLogicos {
+
+public static void main(String[] args) {
+    
+        boolean condicao1 = true;
+        boolean condicao2 =false;
+
+
+        // Operador E
+
+        if (condicao1 && condicao2) {
+            System.out.println("As duas condições são iguais");
+        } else {
+            System.out.println("As condições não são iguais");
+        }
+
+        // Operador OU
+
+        if (condicao1 || condicao2) {
+            System.out.println("As duas condições são iguais");
+        } else {
+            System.out.println("As condições não são iguais");
+        }
+
+
+
+
+    }  
+}
