6a8f91602ac608ec29bb1d71bfa8ba2c6ceceb54
Valmir <valmirleitejunior@gmail.com>
2023-04-03 07:10:53 -0300

    Aula de incremente

--- tipos-variaveis/src/Incremento.java
+++ tipos-variaveis/src/Incremento.java
@@ -1,3 +1,5 @@
+import javax.xml.crypto.dsig.spec.HMACParameterSpec;
+
 public class Incremento {
     public static void main(String[] args) {
         
@@ -10,11 +12,25 @@ public class Incremento {
         System.out.println(numero --);
 
 
+        int c = 5;
+        int d = -- c;
+        System.out.println(d); // imprime 4
+
+        int g = 5;
+        int h = g --;
+        System.out.println(h); // imprime 5
+        System.out.println(g); // imprime 4
+
+        int e = 5;
+        int f = e ++;
+        System.out.println(f); // imprime 5
+        System.out.println(e); // imprime 6
+
         int a = 5;
         int b = ++ a;
-        System.out.println(b);
+        System.out.println(b); // Imprime 6     
 
-    
+
 
 
     }
