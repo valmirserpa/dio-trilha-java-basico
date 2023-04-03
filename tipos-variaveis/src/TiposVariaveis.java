98d7163dff6bd64d3c91c5bcb8bcabb8bbb083f9
Valmir <valmirleitejunior@gmail.com>
2023-04-03 08:51:12 -0300

    Tipos de Variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,7 +2,12 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         
 
-
+        int numero = 2;
+        char B;
+        String Valmir;
+        float decimal = 3.5f;
+        boolean simNao;
+        
         final int NUMERO = 5; // Quando uma variável possui valor, devemos escrever "final" antes do tipo e nome em caixa alta
         System.out.println(NUMERO);
 
