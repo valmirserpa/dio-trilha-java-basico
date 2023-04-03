f493a39a1fac1c49f6c271cf17cdc47bed6a2481
Valmir Conrado Serpa <110781631+valmirserpa@users.noreply.github.com>
2023-04-03 11:58:30 -0300

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,30 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Tv ligada: " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Status da tv: " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Status da tv: " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: "+smartTv.volume);
+
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.novoCanal(13);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+
+
+
+    }
+}
