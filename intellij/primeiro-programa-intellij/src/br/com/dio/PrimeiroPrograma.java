e43c5c7fdae8cd026795d1b630d3830568261bfd
Valmir <valmirleitejunior@gmail.com>
2023-04-11 20:22:11 -0300

    primeiro comit

---
+++ intellij/primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        int a = 5;
+        int b = 3;
+
+        System.out.println("Olá, mundo!" + (a + b));
+
+    }
+}
