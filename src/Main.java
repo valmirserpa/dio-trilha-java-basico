de0deef56c858916636c96409b9cac4922e67865
Valmir <valmirleitejunior@gmail.com>
2023-04-12 16:30:50 -0300

    Estrutura de repetição While

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+        /*Faça um programa que peça uma nota, entre zero e dez.
+        Mostre uma mensagem caso o valor seja inválido
+        e continue pedindo até que o usuário informe um valor válido.*/
+
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota ");
+        nota = scan.nextInt();
+
+        while (nota < 0 | nota >10){
+            System.out.println("Nota inválida, digite novamente");
+             nota = scan.nextInt();
+
+        }
+
+
+    }
+
+}
