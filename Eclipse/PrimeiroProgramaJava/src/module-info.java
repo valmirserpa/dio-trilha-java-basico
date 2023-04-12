bf6e5b8eed270bef84813f5eeab0c568f9476773
Valmir <valmirleitejunior@gmail.com>
2023-04-12 18:40:53 -0300

    PrimeiroCommit

---
+++ Eclipse/PrimeiroProgramaJava/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author valmi
+ *
+ */
+module PrimeiroProgramaJava {
+}
---
+++ intellij/AprendendoIncremento/src/Main.java
@@ -0,0 +1,37 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        // PÓS-INCREMENTO
+
+        int a = 10;
+        int b = a++;
+
+        System.out.println(b);
+        System.out.println(a);
+
+        System.out.println();
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        System.out.println();
+
+
+        // PRÉ- INCREMENTO
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+        System.out.println();
+
+        int c = 5;
+        int d = ++c;
+
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
